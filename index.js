41d6837a15bef27501f9a4bb799b74b30125e1b1
muhammadumar0820 <salimjonovmuhammadumar372@gmail.com>
2023-12-16 18:53:18 +0300

    vazifalar bajarildi

---
+++ index.js
@@ -0,0 +1,43 @@
+//Birinchi masala
+//let a = 1;
+//let b = 5;
+//let sum = 1;
+//let i = a;
+//while(i <= b){
+//    sum = sum * i;
+//    i++;
+//}
+//console.log(sum);
+
+
+//Ikkinchi masala
+//let num = 123;
+//let counter = 0;
+//while(num >= 1){
+//    num=Math.trunc(num/10);
+//    counter++;
+//}
+//console.log(counter);
+
+
+
+//Uchinchi masala
+//let num = 3;
+//let i = 1;
+//while(i >= num){
+//    num*=i;
+//    i++; 
+//}
+//console.log(num);
+
+
+
+//To'rtinchi masala
+//let p = prompt ("Parolni kiriting");
+//while(p == 12345){
+//    break;
+//}
+//if(p!=12345){
+//    prompt("Parolni kiriting")
+//}
+//console.log(p);
